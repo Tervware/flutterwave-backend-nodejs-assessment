6133122049095454830d96280e32c155b8502bad
terver <timothy.chahur@scelloo.com>
2021-01-30 19:18:05 +0100

    :bug: fix validation

--- src/validations.ts
+++ src/validations.ts
@@ -1,6 +1,6 @@
 import { RequestData, ValidationData } from "./types";
 
-export function validateRequestBody(payload: RequestData): any {
+export function validateRequestBody(payload: RequestData): void {
   let { rule, data } = payload;
 
   // rule field is required
@@ -12,43 +12,51 @@ export function validateRequestBody(payload: RequestData): any {
   if (!payload.hasOwnProperty("data")) {
     throw new Error("data is required.");
   }
+  // Data can be an object, string or array
+  if (!(data === Object(data) || typeof data === "string")) {
+    throw new Error("data should be an object, array or a string.");
+  }
 
   // rule must be a valid JSON
   if (!(rule === Object(rule) && !Array.isArray(rule))) {
     throw new Error("rule should be an object.");
   }
 
-  const { field, condition, condition_value } = rule;
+  const { field, condition } = rule;
 
   // VALIDATE FIELD
-  //  field is required
-  if (!rule.hasOwnProperty("field")) {
+  if (typeof field !== "string" && rule.hasOwnProperty("field")) {
+    throw new Error(`rule.field should be a string.`);
+  }
+  //  rule field is required
+  if (!rule.hasOwnProperty("field") || field.trim() === "" ) {
     throw new Error("rule.field is required.");
   }
 
-  if (typeof field !== "string") {
-    throw new Error(`rule.field should be a string.`);
-  }
   // Split field levels into array
-  const fieldLevels: string[] = field.split(".");
+  const field_levels: string[] = field.split(".");
 
   // ensure nesting is not more than two levels
-  if (fieldLevels.length > 2) {
+  if (field_levels.length > 2) {
     throw new Error("nested field object must not exceed two levels.");
   }
   // Check if the field in the data passed to validate the rule against
-  if (!data[fieldLevels[0]]) {
-    throw new Error(`field ${fieldLevels[0]} is missing from data.`);
+  if (!data[field_levels[0]]) {
+    throw new Error(`field ${field_levels[0]} is missing from data.`);
   }
 
-  if (fieldLevels.length === 2 && !data[fieldLevels[0]][fieldLevels[1]]) {
-    throw new Error(`field ${fieldLevels[0]}.${fieldLevels[1]} is missing from data.`);
+  if (field_levels.length === 2 && !data[field_levels[0]][field_levels[1]]) {
+    throw new Error(`field ${field_levels[0]}.${field_levels[1]} is missing from data.`);
   }
 
   // VALIDATE CONDITION
+  if (typeof condition !== "string" && rule.hasOwnProperty("condition") ) {
+    throw new Error(`rule.condition should be a string.`);
+  }
+
   const conditions: string[] = ["eq", "neq", "gt", "gte", "contains"];
   // condition  is required
-  if (!rule.hasOwnProperty("condition")) {
+  if (!rule.hasOwnProperty("condition") || condition.trim() === "") {
     throw new Error("rule.condition is required.");
   }
   // condition  must be  valid
@@ -62,17 +70,12 @@ export function validateRequestBody(payload: RequestData): any {
 
   // VALIDATE CONDITION VALUE
   // condition  is required
-  if (!rule.hasOwnProperty("condition_value")) {
+  if (!rule.hasOwnProperty("condition_value") ) {
     throw new Error("rule.condition_value is required.");
   }
 
-  // VALIDATE DATA
-  // Data can be an object, string or array
-  if (!(data === Object(data) || typeof data === "string")) {
-    throw new Error("data should be an object, array or a string.");
-  }
+ 
 
-  return;
 }
 
 export function isInValidRule(validation_data: ValidationData): any {
